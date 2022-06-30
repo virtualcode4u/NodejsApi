8720564b5251c247c127277c3a7dc46e2cc93ce9
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-06-30 10:38:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const express=require('express');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-app.use(express.json());
+//app.use(express.json());
+
+
+//const dbUrl = 'mongodb+srv://admin:D9ihAcWWH1N6vAlG@cluster0.2wp8ctn.mongodb.net/onlinequiz?retryWrites=true&w=majority';
+
+//Routes = 
+//const category = require('./routes/category');
+
+//Database Connection
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(()=>{
+console.info("Databaese connected Start Working")
+}).catch((e)=>{
+	console.log("Error :", e)
+})
+
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes
+//app.use('/api',category);
 
 
 app.get('/',(req,res)=>{
-	res.send('Welcome to NodejsApi');
+	res.send('Welcome to NodeQuiz');
 });
 
 
