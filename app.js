a11f71c9c15f974a1c2bc36ac070a34d24ab396b
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-06-30 11:13:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 //const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const app = express();
 //app.use(express.json());
 
@@ -26,7 +26,7 @@ const app = express();
 
 //Middleware
 //app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 
 //Routes
 //app.use('/api',category);
