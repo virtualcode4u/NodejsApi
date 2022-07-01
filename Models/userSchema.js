2095fba9fc567a255ac088c9fe4c8f4e3104e5d6
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-01 21:13:53 +0530

    Update userSchema.js

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -2,24 +2,23 @@ const mongoose = require('mongoose');
 
 const schema = mongoose.Schema;
 const userSchema = new schema({
-name:{
-type:String,
-required:true
-},
-mobileno:{
-type:Number,
-required:true,
-},
-email:{
-type:String,
-unique:true,
-required:true
-},
-password:{
-type:String,
-required:true
-},
+    name:{
+        type:String,
+        required:true
+    },
+    mobileno:{
+        type:Number,
+        required:true
+    },
+    email:{
+        type:String,
+         unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+})
 
-});
-
-module.exports = mongoose.model('regusers',userSchema);
+module.exports = mongoose.model('Registeruser',userSchema )
