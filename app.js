6d528c0ebfe9a725d46a35951b3d0513c618c35b
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-01 08:55:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-//require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-//const bodyParser = require('body-parser');
-//const cors = require('cors');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 //app.use(express.json());
 
 
-// const dbUrl = 'mongodb+srv://admin:D9ihAcWWH1N6vAlG@cluster0.2wp8ctn.mongodb.net/onlinequiz?retryWrites=true&w=majority';
+//const dbUrl = 'mongodb+srv://admin:D9ihAcWWH1N6vAlG@cluster0.2wp8ctn.mongodb.net/onlinequiz?retryWrites=true&w=majority';
 
 //Routes = 
 //const category = require('./routes/category');
 
 //Database Connection
-// console.log(process.env.MONGO_URL)
-// mongoose.connect(dbUrl,{
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// }).then(()=>{
-// console.info("Databaese connected Start Working")
-// }).catch((e)=>{
-// 	console.log("Error :", e)
-// })
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(()=>{
+console.info("Databaese connected Start Working")
+}).catch((e)=>{
+	console.log("Error :", e)
+})
 
 
 //Middleware
-//app.use(bodyParser.json());
-//app.use(cors());
+app.use(bodyParser.json());
+app.use(cors());
 
 //Routes
 //app.use('/api',category);
