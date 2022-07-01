51c38575803f7704056d9fbc497c5a1e6b1c6ae2
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-01 12:34:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post("/getsum", function(req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
     
-  var result = num1 + num2 ;
+  var result = num1 ;
     
   res.send("Addition - " + result);
 })
