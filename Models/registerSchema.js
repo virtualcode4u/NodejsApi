50aa195dbefbf3c4e70b5c8ab8af87c7bc93ce7b
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-01 20:38:19 +0530

    Update registerSchema.js

--- Models/registerSchema.js
+++ Models/registerSchema.js
@@ -1,1 +1,25 @@
-ok
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+const userSchema = new Sche({
+name:{
+type:String,
+required:true
+},
+mobile:{
+type:Number,
+required:true,
+}
+email:{
+type:String,
+unique:true,
+required:true
+},
+password:{
+type:String,
+required:true
+},
+
+});
+
+module.exports = mongoose.model('RegisterUser',userSchema);
