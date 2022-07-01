c82bb7a3e8b0d3ba56b2068e48bc398984538601
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-01 21:10:52 +0530

    Update userSchema.js

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
+const schema = mongoose.Schema;
+const userSchema = new schema({
 name:{
 type:String,
 required:true
