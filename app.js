d7a7405c40278e7eee273b85bbd36154eb268866
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-01 10:41:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ mongoose.connect(process.env.MONGO_URL,{
 	app.get('/',(req,res)=>{
 	res.send('Welcome to MongoDB');
 });
+	
+app.post("/getsum", function(req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+    
+  var result = num1 + num2 ;
+    
+  res.send("Addition - " + result);
+})
+	
 console.log("Databaese connected Start Working")
 }).catch((e)=>{
 	console.log("Error :", e)
