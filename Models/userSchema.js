226de91d0af365aedeedc17825207ff4870b1a1f
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-06 10:28:58 +0530

    done

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -23,34 +23,7 @@ const userSchema = new schema({
         type:String,
         required:true
     },
-    state:{
-        type:String,
-        required:false
-    },
-    city:{
-        type:String,
-        required:false
-    },
-    status:{
-        type:String,
-        require:false
-    },
-    created_by: {
-        type: String, 
-        required:true
-    },
-    created_at: {
-        type: Date, 
-        default: Date.now
-    },
-    updated_by: {
-        type: String, 
-        required:true
-    },
-    updated_at: {
-        type: Date, 
-        default: Date.now
-    },    
+
 })
 
 module.exports = mongoose.model('user',userSchema )
