a6b168a6caf8d1608f8e301223697b025212556c
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-06 10:36:32 +0530

    done

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -2,11 +2,35 @@ const mongoose = require('mongoose');
 
 const schema = mongoose.Schema;
 const userSchema = new schema({
-    firntname:{
+    catname:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    catdesc:{
         type:String,
         required:true
+    },
+    isrecclsd:{
+        type:String,
+    },
+    created_by: {
+        type: String, 
+        required:false,
+    },
+    created_at: {
+        type: Date, 
+        default: Date.now
+    },
+    updated_by: {
+        type: String, 
+        required:false,
+    },
+    updated_at: {
+        type: Date, 
+        default: Date.now
     }
-
 })
+
 
 module.exports = mongoose.model('user',userSchema )
