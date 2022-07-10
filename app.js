82fb5fee389708f767be77be7b57091308c0307f
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-10 10:14:28 +0530

    done

--- app.js
+++ app.js
@@ -176,9 +176,7 @@ app.post('/login',async(req,res)=>{
       // check user password with hashed password stored in the database
       const validPassword = await bcrypt.compare(body.password, user.password);
       if (validPassword) {
-        let user = await userSchema({
-            email
-        });
+        
         Jwt.sign({user},jwtKey, {expiresIn:"1h"},(err,token)=>{
             if(err){
                 res.send("Something went wrong try again later!")
