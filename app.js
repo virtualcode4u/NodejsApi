7ee87c1ef689e76f273667799e5a3dc6f8a49880
Raja Sahani <90952992+sahaniraja@users.noreply.github.com>
2022-07-10 09:36:03 +0530

    done

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ app.post('/register',async(req,res)=>{
 app.post('/login',async(req,res)=>{
     try{
     const body = req.body;
-    const user = await userSchema.findOne({ email: body.email },{firstname:1});
+    const user = await userSchema.findOne({ email: body.email }).select({firstname:1});
     if (user) {
       // check user password with hashed password stored in the database
       const validPassword = await bcrypt.compare(body.password, user.password);
